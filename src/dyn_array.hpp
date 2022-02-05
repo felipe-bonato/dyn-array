0e755b5185d9141634edabb1ab1b5394d5d78766
Felipe Bonato <felipe13.bonato@gmail.com>
2022-02-05 17:03:50 -0300

    Added helpers for allocating and accessing the data

--- src/dyn_array.hpp
+++ src/dyn_array.hpp
@@ -12,6 +12,93 @@ private:
 
 
 
+	void grow_if_needed()
+	{
+		// The "-1" is to give some headroom, and for a simpler dislocate_right
+		if(m_len >= m_capacity - 1) grow();
+	}
+
+
+
+	void grow()
+	{
+		realloc_data(m_capacity * 2);
+	}
+
+
+
+	void shrink_if_needed()
+	{
+		if(m_capacity > 16 && m_len <= m_capacity / 4) shrink();
+	}
+
+
+
+	void shrink()
+	{
+		realloc_data(m_capacity / 2);
+	}
+
+
+
+	void realloc_data(size_t new_capacity)
+	{
+		data_t *new_data_ptr = new data_t[new_capacity];
+		copy_content(m_data_ptr, new_data_ptr, m_len);
+		delete[] m_data_ptr;
+		m_data_ptr = new_data_ptr;
+		m_capacity = new_capacity;
+	}
+
+
+
+	void copy_content(data_t *from, data_t *to, size_t content_len)
+	{
+		for(size_t i = 0; i < content_len; ++i){
+			to[i] = from[i];
+		}
+	}
+
+
+
+	void dislocate_right(size_t from_index)
+	{
+		// To make sure we have the space for the last item
+		++m_len;
+		grow_if_needed();
+		--m_len;
+
+		if(from_index + 1 > m_len) std::length_error("Cannot non-continuosly dislocate");
+
+		// Casting to int64_t is needed cause size_t is unsigned
+		// and thus, dislocating from index 0 causes it to underflow
+		for(int64_t i = static_cast<int64_t>(m_len - 1);
+			i >= static_cast<int64_t>(from_index);
+			--i
+		){
+			m_data_ptr[i + 1] = m_data_ptr[i];
+		}
+	
+		m_data_ptr[from_index] = 0;
+		m_len++;
+	}
+
+
+
+	void dislocate_left(size_t from_index)
+	{
+		if(from_index + 1 > m_len) std::length_error("Cannot non-continuosly dislocate");
+
+		for(size_t i = from_index; i < m_len; ++i){
+			m_data_ptr[i] = m_data_ptr[i + 1];
+		}
+	
+		--m_len;
+		shrink_if_needed();
+	}
+
+
+
 public:
 	Dyn_array(size_t initial_size = 16) :
 		m_data_ptr{new data_t[initial_size]},
@@ -30,6 +117,18 @@ public:
 		m_capacity = 0;
 	}
 
+
+
+	data_t at(int64_t position)
+	{
+		position = position < 0 ? static_cast<int64_t>(m_len) + position : position;
+
+		// Checking for position < 0 in case position is less than -m_len
+		if(position >= static_cast<int64_t>(m_len) || position < 0){
+			throw std::length_error("Cannot access item at position outside array");
+		}
+		return m_data_ptr[position];
+	}
 
 
 	std::string as_string()
