751ff530d6c647d2606422508a3240cbe66076fb
Felipe Bonato <felipe13.bonato@gmail.com>
2022-02-05 17:00:53 -0300

    Added helper and as_string methods

--- src/dyn_array.hpp
+++ src/dyn_array.hpp
@@ -1,3 +1,6 @@
+#include<sstream>
+#include<string>
+#include<typeinfo>
 
 template<typename data_t>
 class Dyn_array
@@ -25,6 +28,51 @@ public:
 		delete[] m_data_ptr;
 		m_len = 0;
 		m_capacity = 0;
+	}
+
+
+
+	std::string as_string()
+	{
+		std::stringstream ret_buffer{""};
+
+		//typeid(data_t).name() needs specific compiler flags to work
+		ret_buffer << "dyn_array<" << typeid(data_t).name() << "> @ " << &m_data_ptr << "\n";
+
+		ret_buffer << "\tlen: " << get_len() << "\n";
+
+		ret_buffer << "\tdata: [";
+		for (size_t i = 0; i < m_len; ++i){
+			ret_buffer << m_data_ptr[i];
+			if(i != m_len - 1){
+				ret_buffer << ", "; 
+			}
+
+		}
+		ret_buffer << "]";
+
+		return ret_buffer.str();
+	}
+
+
+
+	inline size_t get_len()
+	{
+		return m_len;
+	}
+
+
+
+	inline size_t get_capacity()
+	{
+		return m_capacity;
+	}
+
+
+
+	inline bool is_empty()
+	{
+		return m_len == 0;
 	}
 };
 
